62bf8184ef5e88ed77fcafc0b17f6f78e1842308
ValriuS <ValriuS@ADMINISTRATOR>
2022-11-01 22:04:19 +0300

    Добавьте файлы проекта.

---
+++ Практика/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Практика
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ВВедите число");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int s= 0;
+            for (int i = 1; i <= n; i++)
+            {
+                s += 2 * i;
+            }
+            Console.WriteLine(s);
+            Console.ReadKey();
+        }
+    }
+}
